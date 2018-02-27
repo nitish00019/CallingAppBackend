9bf64e0363c3cb9f908034e215bcf6f0e3689c31
nitish00019 <nitiskr00019@gmail.com>
2018-02-27 20:39:47 +0530

    first commit

---
+++ src/java/Server.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+
+/**
+ *
+ * @author NITISH ROY
+ */
+public class Server extends HttpServlet {
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, Exception {
+        response.setContentType("text/html;charset=UTF-8");
+        String mobileNumber = request.getParameter("mobile");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/app_db","root","root");
+            PreparedStatement statement = con.prepareStatement("select user_name,email from callers WHERE phone_number = ?");
+            statement.setString(1,mobileNumber);
+          
+            ResultSet result = statement.executeQuery();
+            String JsonResponse = "{";
+            if(result.first()){
+                JsonResponse += "\"status\":\"success\"";
+                JsonResponse += ",";
+                JsonResponse += "\"name\":\""+result.getString(1)+"\"";
+                JsonResponse += ",";
+                JsonResponse += "\"email\":\""+result.getString(2)+"\"";
+            }
+            else{
+                JsonResponse += "\"status\":\"failed\"";
+            }
+           
+
+            JsonResponse += "}";
+
+            out.print(JsonResponse);            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" d esc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try{
+            processRequest(request, response);
+        }
+        catch(Exception e){
+            System.out.println("Error: "+ e.getMessage());
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        processRequest(request, response);
+//    }
+//
+//    /**
+//     * Returns a short description of the servlet.
+//     *
+//     * @return a String containing servlet description
+//     */
+//    @Override
+//    public String getServletInfo() {
+//        return "Short description";
+//    }// </editor-fold>
+
+}
